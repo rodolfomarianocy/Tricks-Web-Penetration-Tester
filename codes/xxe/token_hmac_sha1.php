33378debc7390fefaa54352dd3d839fc3f9f0f74
Rodolfo Mariano <rodolfomarianocy@gmail.com>
2022-07-12 00:12:32 -0300

    Delete token_hmac_sha1.php

--- codes/xxe/token_hmac_sha1.php
+++
@@ -1,5 +0,0 @@
-<?php
-$object = "OBJECT-PHPGGC";
-$secretKey = "SECRET-KEY";
-$cookie = urlencode('{"token":"' . $object . '","sig_hmac_sha1":"' . hash_hmac('sha1', $object, $secretKey) . '"}');
-echo $cookie;
